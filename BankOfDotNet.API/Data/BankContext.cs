9043312abe09322c7d239caf48fe3463f90e3c98
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2024-03-01 22:15:04 -0500

    Add project files.

---
+++ BankOfDotNet.API/Data/BankContext.cs
@@ -0,0 +1,12 @@
+﻿using BankOfDotNet.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankOfDotNet.API.Data
+{
+    public class BankContext: DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options):base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ BankOfDotNet.API/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankOfDotNet.API.Models
+{
+    public class Customer
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
